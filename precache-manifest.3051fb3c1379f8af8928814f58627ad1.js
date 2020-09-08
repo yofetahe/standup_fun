4dd0cfc7ce6475dcfa8c7d07c4a60ea24fccbd34
Yofetahe Habtu <yofetahe@gmail.com>
2020-09-07 23:53:58 -0700

    Updates

---
+++ precache-manifest.3051fb3c1379f8af8928814f58627ad1.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7a337762cb695c1dbba782e6dfa267c",
+    "url": "/standup_fun/index.html"
+  },
+  {
+    "revision": "eb94ff38fc1ec3c13ca9",
+    "url": "/standup_fun/static/css/2.c132a2ce.chunk.css"
+  },
+  {
+    "revision": "54396ccc43a073cc2e56",
+    "url": "/standup_fun/static/css/main.b52cec10.chunk.css"
+  },
+  {
+    "revision": "eb94ff38fc1ec3c13ca9",
+    "url": "/standup_fun/static/js/2.77dc88a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/standup_fun/static/js/2.77dc88a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54396ccc43a073cc2e56",
+    "url": "/standup_fun/static/js/main.46caa621.chunk.js"
+  },
+  {
+    "revision": "d1d7210bb0121ed357fb",
+    "url": "/standup_fun/static/js/runtime-main.ad30bf72.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/standup_fun/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/standup_fun/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/standup_fun/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/standup_fun/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/standup_fun/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/standup_fun/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/standup_fun/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/standup_fun/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/standup_fun/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/standup_fun/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/standup_fun/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/standup_fun/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/standup_fun/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/standup_fun/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/standup_fun/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/standup_fun/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/standup_fun/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "f4ddfc1d0fac5423bf0ff5b28ce3015c",
+    "url": "/standup_fun/static/media/sound_1.f4ddfc1d.gif"
+  },
+  {
+    "revision": "747567965d1ff891d6f34e43c2f06258",
+    "url": "/standup_fun/static/media/sound_2.74756796.gif"
+  },
+  {
+    "revision": "ce634a6f5c5efef7cee7e552f7bb6666",
+    "url": "/standup_fun/static/media/sound_3.ce634a6f.gif"
+  },
+  {
+    "revision": "7b4404f5f44868a89a682f91568fa024",
+    "url": "/standup_fun/static/media/sound_4.7b4404f5.gif"
+  },
+  {
+    "revision": "8a17bddf28813bea1220ac350ab3eb3a",
+    "url": "/standup_fun/static/media/sound_5.8a17bddf.gif"
+  },
+  {
+    "revision": "acf57c98f3dc1489533c92f9e10630c6",
+    "url": "/standup_fun/static/media/sound_6.acf57c98.gif"
+  },
+  {
+    "revision": "e1ee14a36518e727d1c86e21c162c4f4",
+    "url": "/standup_fun/static/media/sound_7.e1ee14a3.gif"
+  },
+  {
+    "revision": "393a3ea01a07493f3c53b634406f8a46",
+    "url": "/standup_fun/static/media/sound_8.393a3ea0.gif"
+  },
+  {
+    "revision": "dac03de3ed46cdbb8369783cfebae720",
+    "url": "/standup_fun/static/media/sound_9.dac03de3.gif"
+  }
+]);
